751e97538ef13c000aea8418396a01cf1de8c4ae
dnmore <morettidn@gmail.com>
2026-01-28 17:21:25 +0100

    setup database and prisma, fetch data

--- app/dashboard/deals/page.tsx
+++ app/dashboard/deals/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-  return <h1 className="text-2xl font-semibold">Deals</h1>;
+import prisma from "@/lib/prisma";
+
+export default async function Page() {
+  const allDeals = await prisma.deal.findMany({
+    select: {
+      id: true,
+      name: true,
+      amount: true,
+      stage: true,
+      lead: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Deals</h1>
+      <ul>
+        {allDeals.map((deal) => (
+          <li key={deal.id}>
+            {deal.name} – {deal.amount} – {deal.stage} – Lead:
+            {deal.lead.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
--- app/dashboard/leads/page.tsx
+++ app/dashboard/leads/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-  return <h1 className="text-2xl font-semibold">Leads</h1>;
+
+import prisma from "@/lib/prisma";
+
+export default async function Page() {
+  const allLeads = await prisma.lead.findMany({
+    select: {
+      id: true,
+      name: true,
+      company: true,
+      email: true,
+      _count: {
+        select: {
+          deals: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold">Leads</h1>
+      <ul>
+        {allLeads.map((lead) => (
+          <li key={lead.id}>
+            {lead.name} – {lead.company} – {lead.email} –{" "}
+            Deals: {lead._count.deals}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
