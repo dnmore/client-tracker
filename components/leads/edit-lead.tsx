091aa65db58032bc9a29db46f4dafa1c4bedb303
dnmore <morettidn@gmail.com>
2026-02-05 11:17:02 +0100

    update edit lead

--- components/leads/edit-lead.tsx
+++ components/leads/edit-lead.tsx
@@ -9,10 +9,10 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
-import { Lead } from "@/lib/definitions";
+import { LeadTableRaw } from "@/lib/definitions";
 import { updateLead, LeadState } from "@/lib/actions";
 
-export default function EditLeadForm({ lead }: { lead: Lead }) {
+export default function EditLeadForm({ lead }: { lead: LeadTableRaw }) {
   const initialState: LeadState = {
     message: null,
     errors: {},
