4a9ccfa02833891daf4cc00f180b6ee37c222a37
dnmore <morettidn@gmail.com>
2026-08-31 17:13:15 +0200

    update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Footer from "@/components/layout/footer";
+import {Footer} from "@/components/layout/footer";
 
 const inter = Inter({
-  subsets: ["latin"]
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -24,19 +24,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.className} antialiased min-h-screen flex flex-col`}
-      >
+      <body className={`${inter.className} antialiased min-h-screen`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        ><main>
-            <div className="flex-1 flex flex-col">{children}</div>
-        </main>
-        
-          <Footer />
+        >
+          
+          <div className="min-h-screen flex flex-col">
+            <main className="flex-1">{children}</main>
+
+            <Footer />
+          </div>
         </ThemeProvider>
       </body>
     </html>
