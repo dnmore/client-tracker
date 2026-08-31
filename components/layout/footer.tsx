f647061572340ecbb58c17a573f885def8aad10c
dnmore <morettidn@gmail.com>
2026-08-31 17:21:29 +0200

    update footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,17 +1,16 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="w-full border-t py-4 px-6 flex items-center flex-wrap gap-2">
+    <footer className="w-full border-t py-4 px-6 flex items-center flex-wrap gap-2 text-muted-foreground">
       <p className="text-xs md:border-r px-4">
         Track Clients, Deals, Revenue — All in One Place.
       </p>
-      <Button variant="link" asChild>
-        <Link href="https://github.com/dnmore/client-tracker" target="_blank" className="text-xs">
+      
+        <Link href="https://github.com/dnmore/client-tracker" target="_blank" className="text-xs hover:border-b">
           View Source
         </Link>
-      </Button>
+      
     </footer>
   );
 }
